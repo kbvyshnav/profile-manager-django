70794a36d4cd3fe805eaabc9829222ebfe9211bf
Vyshnav K B <vyshnavkb648@gmail.com>
2026-03-23 00:23:57 +0530

    Added Profile model with admin and migrations

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
