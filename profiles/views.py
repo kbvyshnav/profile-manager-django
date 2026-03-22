f11502dd4fbe9ae03207490833ff5c0475f0a633
Vyshnav K B <vyshnavkb648@gmail.com>
2026-03-23 00:03:18 +0530

    Setup professional project structure with global templates and static

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'profiles/home.html')
+
+def add_profile(request):
+    return render(request, 'profiles/add_profile.html')
