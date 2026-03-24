ea894868ea3eeb05f84d3ac6cfcd451645f027e2
Vyshnav K B <vyshnavkb648@gmail.com>
2026-03-24 12:48:25 +0530

    Displayed profiles using ORM with improved UI

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Profile
 
 def home(request):
-    return render(request, 'profiles/home.html')
+    profiles = Profile.objects.filter(is_active=True)
+
+    context = {
+        "profiles": profiles
+    }
+    return render(request, 'profiles/home.html', context)
 
 def add_profile(request):
     return render(request, 'profiles/add_profile.html')
