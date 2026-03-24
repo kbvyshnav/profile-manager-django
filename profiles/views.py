cbad4521e4067d7c72a999e499b21b3a6552f078
Vyshnav K B <vyshnavkb648@gmail.com>
2026-03-24 23:56:36 +0530

    Implemented profile creation using ModelForm with UI improvements

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 def home(request):
     profiles = Profile.objects.filter(is_active=True)
@@ -10,4 +13,16 @@ def home(request):
     return render(request, 'profiles/home.html', context)
 
 def add_profile(request):
-    return render(request, 'profiles/add_profile.html')
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile added successfully ✅")
+            return redirect('home')
+
+    else:
+        form = ProfileForm()
+
+    return render(request, 'profiles/add_profile.html', {'form': form})
