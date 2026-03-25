b77983e9674dc7c548bd858791c41d12a0bfbaf0
Vyshnav K B <vyshnavkb648@gmail.com>
2026-03-25 13:20:53 +0530

    Implemented update profile functionality with pre-filled form

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-profile/', views.add_profile, name='add_profile'),
+    path('update-profile/<int:id>', views.update_profile, name='update_profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from .models import Profile
 from .forms import ProfileForm
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
 
 def home(request):
     profiles = Profile.objects.filter(is_active=True)
@@ -24,5 +25,21 @@ def add_profile(request):
 
     else:
         form = ProfileForm()
+
+    return render(request, 'profiles/add_profile.html', {'form': form})
+
+def update_profile(request, id):
+    profile = get_object_or_404(Profile, id=id) # safely fetches object, avoids crash if not found
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile) # loads existing data, updates record instead of creating new
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully ✏️")
+            return redirect('home')
+
+    else:
+        form = ProfileForm(instance=profile) # loads existing data into form(pre-filled)
 
     return render(request, 'profiles/add_profile.html', {'form': form})
